21cd8b948b37529cb5c9df629245a9e6e8992819
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-20 21:20:23 +0300

    updated: spinner (combo box)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.ismailmesutmujde.kotlinspinner"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.ismailmesutmujde.kotlinspinner"
         minSdk = 24
--- app/src/main/java/com/ismailmesutmujde/kotlinspinner/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinspinner/MainActivity.kt
@@ -1,13 +1,54 @@
 package com.ismailmesutmujde.kotlinspinner
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.ismailmesutmujde.kotlinspinner.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bindingMain : ActivityMainBinding
+    private lateinit var dataAdapter : ArrayAdapter<String>
+    private val countries = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMain = ActivityMainBinding.inflate(layoutInflater)
+        val view = bindingMain.root
+        setContentView(view)
 
+        countries.add("Türkiye")
+        countries.add("Italy")
+        countries.add("Germany")
+        countries.add("Japan")
+        countries.add("China")
+        countries.add("Portugal")
+        countries.add("Spain")
+
+        dataAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, android.R.id.text1, countries)
+        bindingMain.spinner.adapter = dataAdapter
+
+        bindingMain.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(applicationContext,"Selected Country : ${countries[position]}", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
+
+        bindingMain.buttonShow.setOnClickListener {
+            Toast.makeText(applicationContext,"Country : ${countries[bindingMain.spinner.selectedItemPosition]}", Toast.LENGTH_LONG).show()
+        }
     }
 }
